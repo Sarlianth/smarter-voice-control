13c200070e8209a52a23f5b7c6e4d18ee2d96e29
Adrian Sypos <adrian00935@gmail.com>
2018-03-15 16:39:21 +0000

    Connected successfully to smarter. Brewing coffee method working perfectly as well as restarting the machine to default settings

--- smarter.py
+++ smarter.py
@@ -1,47 +1,65 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#imports
 import sys
 import socket
 import json
-import binascii
 
-#defining method names
-METHOD_BREW = "brew"
-METHOD_RESET = "reset"
-api_method = " "
-msg = " "
-message_to_send = ""
-success = ""
-message = ""
+#method names
+API_METHOD_BREW = "brew"
+API_METHOD_RESET = "reset"
 
-#ip address and port number to connect to coffee maker
+return_code = ""
+
+#IP address of the smarter
 TCP_IP = '172.16.1.91'
 TCP_PORT = 2081
 BUFFER_SIZE = 10
 
-if api_method == METHOD_BREW:
-	message_to_send = "7"
+#default method to call
+api_method = sys.argv[1]
 
-elif api_method == METHOD_RESET:
+if api_method == API_METHOD_BREW:
+	message_to_send = "7"
+elif api_method == API_METHOD_RESET:
 	message_to_send = "\x10"
 
-#connect to machine and send message_to_send
+#connection to machine
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((TCP_IP, TCP_PORT))
-	s.send( bytearray(message_to_send, 'utf8') )
+	s.send(str.encode(message_to_send))
 	data = s.recv(BUFFER_SIZE)
 	s.close()
 except (socket.error, msg):
 	print ('Failed. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1])
 	sys.exit();
 
-print (data)
-#translate to unicode
-#return_code = unicode(data)
+#convert response from machine to unicode
+return_code = str(data, 'utf-8')
 
-print (json.dumps({'success': success, 'message': message, 'return_code': repr(data)[1:10]}))
+#default values
+success=0
+message=""
+
+print (return_code)
+
+#find out what the machine response means (not working well)
+if return_code =="\x03\x00~":
+	success=1
+	message="brewing"
+elif return_code=="\x03\x01~":
+	message="already brewing"
+elif return_code=="\x03\x05~":
+	message="no carafe"
+elif return_code=="\x03\x06~":
+	message="no water"
+elif return_code=="\x03i~":
+	success=1
+	message="reset"
+else:
+	message="check machine"
+
+#ouput JSON
+print (json.dumps({'success': success,'message':message,'return_code':repr(data)[1:10]}))
 
 quit()
 sys.exit()
