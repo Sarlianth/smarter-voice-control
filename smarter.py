a4be909c5b149811996d8431041ed0b34c85b572
Adrian Sypos <adrian00935@gmail.com>
2018-03-15 13:06:30 +0000

    connecting to the machine and receiving bytes back. translating it to unicode and displaying to user

--- smarter.py
+++ smarter.py
@@ -19,5 +19,21 @@ if api_method == METHOD_BREW:
 elseif api_method == METHOD_RESET
 	message_to_send = "\x10"
 
+#connect to machine and send message_to_send
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(message_to_send)
+	data = s.recv(BUFFER_SIZE)
+	s.close()
+except socket.error, msg:
+	print 'Failed. Error code: ' + str(msg[0]) + ', Message : ' + msg[1]
+	sys.exit()
+
+#translate to unicode
+return_code = unicode(data)
+
+print json.dumps({'success': success, 'message': message, 'return_code': repr(data)[1:10]})
+
 quit()
 sys.exit()
