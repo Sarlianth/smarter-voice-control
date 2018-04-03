904a1b11ff6b6e9fdeba345c1dde05fa0f7979f4
Adrian Sypos <adrian00935@gmail.com>
2018-04-03 14:21:34 +0100

    Typo fixed

--- CustomCortanaCommands/CustomCortanaCommands/App.xaml.cs
+++ CustomCortanaCommands/CustomCortanaCommands/App.xaml.cs
@@ -124,11 +124,11 @@ namespace CustomCortanaCommands
                 System.Diagnostics.Debug.WriteLine(voiceCommandName);
                 System.Diagnostics.Debug.WriteLine(textSpoken);
 
-                switch(voiceCommandName)
+                switch (voiceCommandName)
                 {
                     case "TurnOn":
                         System.Diagnostics.Debug.WriteLine("Turning on");
-                        
+
                         break;
                     case "TurnOff":
                         System.Diagnostics.Debug.WriteLine("Turning off");
@@ -160,35 +160,36 @@ namespace CustomCortanaCommands
                         break;
                     case "DefaultSetting":
                         System.Diagnostics.Debug.WriteLine("reset to default");
-                        
+
                         break;
                 }
 
+            }
         }
 
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
+            /// <summary>
+            /// Invoked when Navigation to a certain page fails
+            /// </summary>
+            /// <param name="sender">The Frame which failed navigation</param>
+            /// <param name="e">Details about the navigation failure</param>
+            void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+            {
+                throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+            }
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
+            /// <summary>
+            /// Invoked when application execution is being suspended.  Application state is saved
+            /// without knowing whether the application will be terminated or resumed with the contents
+            /// of memory still intact.
+            /// </summary>
+            /// <param name="sender">The source of the suspend request.</param>
+            /// <param name="e">Details about the suspend request.</param>
+            private void OnSuspending(object sender, SuspendingEventArgs e)
+            {
+                var deferral = e.SuspendingOperation.GetDeferral();
+                //TODO: Save application state and stop any background activity
+                deferral.Complete();
+            }
         }
     }
-}
